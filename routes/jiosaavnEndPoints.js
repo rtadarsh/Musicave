444aa58823a8313681eed6e71ef91d89fcea1c9b
rtadarsh <adarshramtripathi123@gmail.com>
2022-05-27 14:40:28 +0530

    Added Now Playing Page

--- routes/jiosaavnEndPoints.js
+++ routes/jiosaavnEndPoints.js
@@ -2,16 +2,40 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
+
 
 hbs.registerHelper('each_upto', function (ary, max, options) {
     if (!ary || ary.length == 0)
         return options.inverse(this);
 
     var result = [];
-    for (var i = 0; i < max && i < ary.length; ++i)
+    for (var i = 0; i < max && i < ary.length; i++)
         result.push(options.fn(ary[i]));
     return result.join('');
 });
+
+var spotifyApi = new SpotifyWebApi({
+    clientId: '10108018aed74ed4aaf69a1e83cd5adb',
+    clientSecret: '8f9cac2afdef49ed9ad0dd37f7598446'
+});
+
+spotifyApi.clientCredentialsGrant().then(
+    function (data) {
+        console.log('The access token is ' + data.body.access_token);
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body.access_token);
+
+        exports.spotifyApi = spotifyApi;
+        console.log(module.exports);
+    },
+    function (err) {
+        console.log(
+            'Something went wrong when retrieving an access token',
+            err.message
+        );
+    }
+);
 
 router.get('/', function (req, res) {
     console.log("jiosaavn api");
@@ -24,6 +48,49 @@ router.get('/', function (req, res) {
                 trending: trending
             });
         });
+});
+
+router.post('/play', async (req, res) => {
+    const recommendedTrackNames = [];
+    const recommendedTracks = [];
+    await spotifyApi.searchTracks(`${req.body.songName} ${req.body.albumName}`)// give track and artist name from jiosaavn api
+        .then(async (data) => {
+            // console.log("searching")
+            const songId = data.body.tracks.items[0].id;// get the song id and feed it to recomm system
+            await spotifyApi.getRecommendations({
+                seed_tracks: [songId]
+            }).then(data => {
+                // console.log("recomm");
+                const tracks = data.body.tracks;
+                for (let i = 0; i < Math.min(tracks.length, 5); i++) {
+                    // console.log(tracks[i].name);
+                    recommendedTrackNames.push(tracks[i].name)
+                }
+                // recommendedTrackNames.forEach(name => console.log(name))
+            }, (err) => console.log(err))
+        });
+    // console.log(recommendedTrackNames.length);
+    for (let i = 0; i < recommendedTrackNames.length; i++) {
+        await axios.get(`https://saavn.me/search/songs?query=${recommendedTrackNames[i]}&page=1&limit=1`)
+            .then(data => {
+                console.log("pushing");
+                recommendedTracks.push(data.data.results[0]);
+            })
+    }
+    // recommendedTrackNames.forEach((trackName) => {
+    //     console.log("fetching");
+    //     axios.get(`https://saavn.me/search/songs?query=${trackName}&page=1&limit=1`)
+    //         .then(async data => {
+    //             console.log("pushing")
+    //             recommendedTracks.push(data.data.results[0]);
+    //         }, err => console.log(err));
+    // });
+    console.log("random");
+    res.render('nowPlaying', {
+        songUrl: req.body.songUrl,
+        recommendedTracks: recommendedTracks
+    })
+    console.log("playing")
 });
 
 router.get('/albumtrack/:alid', function (req, res) {
